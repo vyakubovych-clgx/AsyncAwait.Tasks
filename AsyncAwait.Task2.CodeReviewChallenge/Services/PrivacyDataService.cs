eadbc9b2214bb89d2f431651571e84e6e4b94625
Viktor Yakubovych <vyakubovych@corelogic.com>
2022-06-18 22:16:40 +0300

    Update PrivacyDataService.cs

--- AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
@@ -4,7 +4,7 @@ namespace AsyncAwait.Task2.CodeReviewChallenge.Services;
 
 public class PrivacyDataService : IPrivacyDataService
 {
-    public Task<string> GetPrivacyDataAsync()
-        => Task.FromResult("This Policy describes how async/await processes your personal data," +
-                           "but it may not address all possible data processing scenarios.");
+    public async Task<string> GetPrivacyDataAsync()
+        => "This Policy describes how async/await processes your personal data," +
+                           "but it may not address all possible data processing scenarios.";
 }
