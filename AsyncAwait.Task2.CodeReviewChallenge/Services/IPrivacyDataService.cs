cd74a934c74bb2a4c93da3317aefa09e34200c09
Viktor Yakubovych <vyakubovych@corelogic.com>
2022-06-21 19:04:02 +0300

    Add reading string from file

--- AsyncAwait.Task2.CodeReviewChallenge/Services/IPrivacyDataService.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Services/IPrivacyDataService.cs
@@ -5,4 +5,5 @@ namespace AsyncAwait.Task2.CodeReviewChallenge.Services;
 public interface IPrivacyDataService
 {
     Task<string> GetPrivacyDataAsync();
+    string GetPrivacyData();
 }
--- AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
@@ -1,10 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace AsyncAwait.Task2.CodeReviewChallenge.Services;
 
 public class PrivacyDataService : IPrivacyDataService
 {
+    private readonly string _privacyDataMessageFilePath;
+
+    public PrivacyDataService(IConfiguration configuration)
+    {
+        _privacyDataMessageFilePath = configuration["PrivacyDataMessageFilePath"];
+    }
+
     public async Task<string> GetPrivacyDataAsync()
+        => await File.ReadAllTextAsync(_privacyDataMessageFilePath);
+
+    public string GetPrivacyData()
         => "This Policy describes how async/await processes your personal data," +
-                           "but it may not address all possible data processing scenarios.";
+           "but it may not address all possible data processing scenarios.";
 }
